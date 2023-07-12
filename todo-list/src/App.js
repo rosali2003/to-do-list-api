d93c158ef108aef60cec79851048290b8f366d86
Rosa Li <rosali@Rosas-MacBook-Pro.local>
2023-07-12 09:44:33 -0400

    added completed column

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-// import {Hi} from './components/Hi.tsx'
 import React from 'react'
-// import axios from 'axios';
+import axios from 'axios';
 import Mainpage from './components/Mainpage'
 
-// const API_URL = "http://localhost:3000/api/v1/book";
+const API_URL = "http://localhost:3000/api/v1/book";
 
-// function getAPIData() {
-//   return axios.get(API_URL).then((response) => response.data)
-// }
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
   return (
